8eacff7a8821977fb893856e8f5426b9dd254354
Junghyun An <daniel7806@naver.com>
2023-07-15 01:35:50 +0900

    chage loading app name

--- lib/src/ui/main_loading.dart
+++ lib/src/ui/main_loading.dart
@@ -40,7 +40,7 @@ class _LoadingPageState extends State<LoadingPage> {
             Container(
               alignment: Alignment.bottomCenter,
               child: const Text(
-                '마음의 편지',
+                'Wiro',
                 style: TextStyle(
                   fontSize: 40,
                   fontWeight: FontWeight.w600,
